5a3538a3a61313f618faecb12044b27dbc269c6b
Shashi Gowda <shashi@juliacomputing.com>
2017-12-15 16:12:56 +0530

    for table

--- src/TableView.jl
+++ src/TableView.jl
@@ -3,8 +3,48 @@ module TableView
 using WebIO
 using JuliaDB
 
-import JuliaDB: DNDSparse
+import JuliaDB: DNDSparse, DNextTable, NextTable
 
+function showtable(t::Union{DNextTable, NextTable}; rows=1:100, colopts=Dict(), kwargs...)
+    w = Widget(dependencies=["https://cdnjs.cloudflare.com/ajax/libs/handsontable/0.34.0/handsontable.full.js",
+                             "https://cdnjs.cloudflare.com/ajax/libs/handsontable/0.34.0/handsontable.full.css"])
+    data = Observable{Any}(w, "data", [])
+
+    subt = JuliaDB.subtable(t, rows)
+
+    headers = colnames(subt)
+    cols = [merge(Dict(:data=>n), get(colopts, n, Dict())) for n in headers]
+
+    options = Dict(
+        :data => collect(JuliaDB.rows(subt)),
+        :colHeaders => headers,
+        :modifyColWidth => @js(w -> w > 300 ? 300 : w),
+        :modifyRowHeight => @js(h -> h > 60 ? 50 : h),
+        :manualColumnResize => true,
+        :manualRowResize => true,
+        :columns => cols,
+        :width => 800,
+        :height => 400,
+    )
+    if (length(t.pkey) > 0 && t.pkey == [1:length(t.pkey);])
+        options[:fixedColumnsLeft] = length(t.pkey)
+    end
+
+    merge!(options, Dict(kwargs))
+
+    handler = @js function (Handsontable)
+        @var sizefix = document.createElement("style");
+        sizefix.textContent = """
+            .htCore td {
+                white-space:nowrap
+            }
+        """
+        this.dom.appendChild(sizefix)
+        this.hot = @new Handsontable(this.dom, $options);
+    end
+    ondependencies(w, handler)
+    w()
+end
 function showtable(t::Union{DNDSparse, NDSparse}; rows=1:100, colopts=Dict(), kwargs...)
     w = Widget(dependencies=["https://cdnjs.cloudflare.com/ajax/libs/handsontable/0.34.0/handsontable.full.js",
                              "https://cdnjs.cloudflare.com/ajax/libs/handsontable/0.34.0/handsontable.full.css"])
