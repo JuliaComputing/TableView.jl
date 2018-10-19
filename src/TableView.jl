06ef085ffc4bb980ec6768dec9ff5df0a7978dcc
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2018-10-19 17:11:11 +0200

    use Tables.jl interface and ag-grid

--- src/TableView.jl
+++ src/TableView.jl
@@ -1,119 +1,76 @@
 module TableView
+using Tables, WebIO, JSExpr, JSON, Dates
 
-using WebIO
-using JSExpr
-using JuliaDB
-using DataValues
+function showtable(x; dark = false)
+    w = Scope(imports=["https://unpkg.com/ag-grid-community/dist/ag-grid-community.min.noStyle.js",
+                       "https://unpkg.com/ag-grid-community/dist/styles/ag-grid.css",
+                       "https://unpkg.com/ag-grid-community/dist/styles/ag-theme-balham$(dark ? "-dark" : "").css",])
 
-import JuliaDB: DNDSparse, DNextTable, NextTable
+    schema = Tables.schema(x)
+    names = schema.names
+    types = schema.types
 
-function JuliaDB.subtable(t::DNextTable, r)
-    table(collect(rows(t)[r]), pkey=t.pkey)
-end
-
-showna(xs) = xs
-function showna(xs::AbstractArray{T}) where {T<:DataValue}
-    map(xs) do x
-        isnull(x) ? "NA" : get(x)
-    end
-end
-
-function showna(xs::Columns)
-    rows(map(showna, columns(xs)))
-end
-
-function showtable(t::Union{DNextTable, NextTable}; rows=1:100, colopts=Dict(), kwargs...)
-    w = Scope(imports=["https://cdnjs.cloudflare.com/ajax/libs/handsontable/0.34.0/handsontable.full.js",
-                       "https://cdnjs.cloudflare.com/ajax/libs/handsontable/0.34.0/handsontable.full.css"])
-
-    trunc_rows = max(1, first(rows)):min(length(t), last(rows))
-    subt = JuliaDB.subtable(t, trunc_rows)
-
-    headers = colnames(subt)
-    cols = [merge(Dict(:data=>n), get(colopts, n, Dict())) for n in headers]
+    coldefs = [(
+                    headerName = n,
+                    field = n,
+                    type = types[i] <: Union{Missing, T where T <: Number} ? "numericColumn" : nothing,
+                    filter = types[i] <: Union{Missing, T where T <: Dates.Date} ? "agDateColumnFilter" :
+                             types[i] <: Union{Missing, T where T <: Number} ? "agNumberColumnFilter" : nothing
+               ) for (i, n) in enumerate(names)]
 
     options = Dict(
-        :data => showna(collect(JuliaDB.rows(subt))),
-        :colHeaders => headers,
-        :modifyColWidth => @js(w -> w > 300 ? 300 : w),
-        :modifyRowHeight => @js(h -> h > 60 ? 50 : h),
-        :manualColumnResize => true,
-        :manualRowResize => true,
-        :columns => cols,
-        :width => 800,
-        :height => 400,
+        :rowData => table2json(x),
+        :columnDefs => coldefs,
+        :enableSorting => true,
+        :enableFilter => true,
+        :enableColResize => true,
+        :multiSortKey => "ctrl"
     )
-    if (length(t.pkey) > 0 && t.pkey == [1:length(t.pkey);])
-        options[:fixedColumnsLeft] = length(t.pkey)
-    end
 
-    merge!(options, Dict(kwargs))
-
-    handler = @js function (Handsontable)
-        @var sizefix = document.createElement("style");
-        sizefix.textContent = """
-            .htCore td {
-                white-space:nowrap
-            }
-        """
-        this.dom.appendChild(sizefix)
-        this.hot = @new Handsontable(this.dom, $options);
+    handler = @js function (agGrid)
+        gridOptions = $options
+        gridOptions.rowData = JSON.parse(gridOptions.rowData)
+        this.table = @new agGrid.Grid(this.dom.querySelector("#grid"), gridOptions)
+        gridOptions.columnApi.autoSizeColumns($names)
     end
     onimport(w, handler)
-    w.dom = dom"div"()
+    w.dom = dom"div#grid"(className = "ag-theme-balham$(dark ? "-dark" : "")",
+                          style=Dict(:position => "absolute",
+                                     :top => "0",
+                                     :left => "0",
+                                     :width => "100%",
+                                     :height => "100%",
+                                     :minHeight => "200px"))
     w
 end
 
-function showtable(t::Union{DNDSparse, NDSparse}; rows=1:100, colopts=Dict(), kwargs...)
-    w = Scope(imports=["https://cdnjs.cloudflare.com/ajax/libs/handsontable/0.34.0/handsontable.full.js",
-                       "https://cdnjs.cloudflare.com/ajax/libs/handsontable/0.34.0/handsontable.full.css"])
-    data = Observable{Any}(w, "data", [])
+# directly write JSON instead of allocating temporary dicts etc
+function table2json(table)
+    names = Tables.schema(table).names
 
-    trunc_rows = max(1, first(rows)):min(length(t), last(rows))
-
-    ks = keys(t)[trunc_rows]
-    vs = values(t)[trunc_rows]
-
-    if !isa(keys(t), Columns)
-         ks = collect(ks)
-         vs = collect(vs)
+    io = IOBuffer()
+    print(io, '[')
+    for row in Tables.rows(table)
+        print(io, '{')
+        i = 1
+        for col in Tables.eachcolumn(row)
+            JSON.print(io, names[i])
+            i += 1
+            print(io, ':')
+            if col isa Number
+                JSON.print(io, col)
+            else
+                JSON.print(io, sprint(print, col))
+            end
+            print(io, ',')
+        end
+        skip(io, -1)
+        print(io, '}')
+        print(io, ',')
     end
+    skip(io, -1)
+    print(io, ']')
 
-    subt = NDSparse(showna(ks), showna(vs))
-
-    headers = colnames(subt)
-    cols = [merge(Dict(:data=>n), get(colopts, n, Dict())) for n in headers]
-
-    options = Dict(
-        :data => JuliaDB.rows(subt),
-        :colHeaders => headers,
-        :fixedColumnsLeft => ndims(t),
-        :modifyColWidth => @js(w -> w > 300 ? 300 : w),
-        :modifyRowHeight => @js(h -> h > 60 ? 50 : h),
-        :manualColumnResize => true,
-        :manualRowResize => true,
-        :columns => cols,
-        :width => 800,
-        :height => 400,
-    )
-
-    merge!(options, Dict(kwargs))
-
-    handler = @js function (Handsontable)
-        @var sizefix = document.createElement("style");
-        sizefix.textContent = """
-            .htCore td {
-                white-space:nowrap
-            }
-        """
-        this.dom.appendChild(sizefix)
-        this.hot = @new Handsontable(this.dom, $options);
-    end
-    onimport(w, handler)
-    w.dom = dom"div"()
-    w
+    String(take!(io))
 end
-
-showtable(t; kwargs...) = showtable(table(t); kwargs...)
-
-end # module
+end
