779a0acae83e5c0907b26fe7648f7172a9744fa6
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2018-10-22 13:33:29 +0200

    support TableTraits interface also show coltype as tooltip

--- src/TableView.jl
+++ src/TableView.jl
@@ -1,17 +1,30 @@
 module TableView
-using Tables, WebIO, JSExpr, JSON, Dates
+using Tables, TableTraits, IteratorInterfaceExtensions
+using WebIO, JSExpr, JSON, Dates
 
 function showtable(x; dark = false)
+    if Tables.istable(typeof(x))
+        return _showtable(x, dark)
+    elseif TableTraits.isiterabletable(x)
+        it = IteratorInterfaceExtensions.getiterator(x)
+        return _showtable(Tables.DataValueUnwrapper(it), dark)
+    end
+    throw(ArgumentError("Argument is not a table."))
+end
+
+function _showtable(table, dark)
+    schema = Tables.schema(table)
+    names = schema.names
+    types = schema.types
+    rows = Tables.rows(table)
+
     w = Scope(imports=["https://unpkg.com/ag-grid-community/dist/ag-grid-community.min.noStyle.js",
                        "https://unpkg.com/ag-grid-community/dist/styles/ag-grid.css",
                        "https://unpkg.com/ag-grid-community/dist/styles/ag-theme-balham$(dark ? "-dark" : "").css",])
 
-    schema = Tables.schema(x)
-    names = schema.names
-    types = schema.types
-
     coldefs = [(
                     headerName = n,
+                    headerTooltip = types[i],
                     field = n,
                     type = types[i] <: Union{Missing, T where T <: Number} ? "numericColumn" : nothing,
                     filter = types[i] <: Union{Missing, T where T <: Dates.Date} ? "agDateColumnFilter" :
@@ -19,7 +32,7 @@ function showtable(x; dark = false)
                ) for (i, n) in enumerate(names)]
 
     options = Dict(
-        :rowData => table2json(x),
+        :rowData => table2json(rows, names, types),
         :columnDefs => coldefs,
         :enableSorting => true,
         :enableFilter => true,
@@ -35,22 +48,17 @@ function showtable(x; dark = false)
     end
     onimport(w, handler)
     w.dom = dom"div#grid"(className = "ag-theme-balham$(dark ? "-dark" : "")",
-                          style=Dict(:position => "absolute",
-                                     :top => "0",
-                                     :left => "0",
-                                     :width => "100%",
-                                     :height => "100%",
-                                     :minHeight => "200px"))
+                          style=Dict(:width => "100%",
+                                     "min-width" => "400px",
+                                     :height => "800px"))
     w
 end
 
 # directly write JSON instead of allocating temporary dicts etc
-function table2json(table)
-    names = Tables.schema(table).names
-
+function table2json(rows, names, types)
     io = IOBuffer()
     print(io, '[')
-    for row in Tables.rows(table)
+    for row in rows
         print(io, '{')
         i = 1
         for col in Tables.eachcolumn(row)
