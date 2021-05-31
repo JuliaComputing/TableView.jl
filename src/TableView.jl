89763672c627fd216c7ad63fa914b61bd106556c
pdeffebach <23196228+pdeffebach@users.noreply.github.com>
2021-05-31 19:05:21 -0400

    Update docstring so list displays properly
    Try `? showtable` on `master` and the same on this branch. Just a small fix to make it render better.

--- src/TableView.jl
+++ src/TableView.jl
@@ -53,18 +53,19 @@ showtable(table::AbstractMatrix; kwargs...) = showtable(Tables.table(table); kwa
 Return a `WebIO.Scope` that displays the provided `table`.
 
 Optional arguments:
-    - `options`: Directly passed to agGrid's `Grid` constructor. Refer to the
-                 [documentation](https://www.ag-grid.com/documentation/) for more info.
-    - `options_mutator!`: Runs on the `options` dictionary populated by TableView and allows for
-                          customizing the grid (at your own risk -- you can break the package by
-                          supplying invalid options).
-    - `dark`: Switch to a dark theme.
-    - `title`: Displayed above the table if non-empty;
-    - `height`/`width`: CSS attributes specifying the output height and with.
-    - `cell_changed`: Either `nothing` or a function that takes a single argument with the fields
-                      `"new"`, `"old"`, `"row"`, and `"col"`. This function is called whenever the
-                      user edits a table field. Note that all values will be strings, so you need to
-                      do the necessary conversions yourself.
+
+- `options`: Directly passed to agGrid's `Grid` constructor. Refer to the
+  [documentation](https://www.ag-grid.com/documentation/) for more info.
+- `options_mutator!`: Runs on the `options` dictionary populated by TableView and allows for
+  customizing the grid (at your own risk -- you can break the package by
+  supplying invalid options).
+- `dark`: Switch to a dark theme.
+- `title`: Displayed above the table if non-empty;
+- `height`/`width`: CSS attributes specifying the output height and with.
+- `cell_changed`: Either `nothing` or a function that takes a single argument with the fields
+  `"new"`, `"old"`, `"row"`, and `"col"`. This function is called whenever the
+  user edits a table field. Note that all values will be strings, so you need to
+  do the necessary conversions yourself.
 """
 function showtable(table;
         options::Dict{Symbol, Any} = Dict{Symbol, Any}(),
